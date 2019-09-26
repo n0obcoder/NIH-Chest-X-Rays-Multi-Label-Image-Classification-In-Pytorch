3704fd3755d509d989be44582ce4edfce3ae709b
n0obcoder <31921968+n0obcoder@users.noreply.github.com>
2019-09-26 11:19:58 +0530

    added gpu support

--- losses.py
+++ losses.py
@@ -3,15 +3,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class FocalLoss(nn.Module):
-
-    def __init__(self, gamma = 1.0):
+    
+    def __init__(self, device, gamma = 1.0):
         super(FocalLoss, self).__init__()
-        self.gamma = torch.tensor(gamma, dtype = torch.float32)
+        self.device = device
+        self.gamma = torch.tensor(gamma, dtype = torch.float32).to(device)
         self.eps = 1e-6
-
+        
+#         self.BCE_loss = nn.BCEWithLogitsLoss(reduction='none').to(device)
+        
     def forward(self, input, target):
         
-        BCE_loss = F.binary_cross_entropy_with_logits(input, target, reduction='none')
+        BCE_loss = F.binary_cross_entropy_with_logits(input, target, reduction='none').to(self.device)
+#         BCE_loss = self.BCE_loss(input, target)
         pt = torch.exp(-BCE_loss) # prevents nans when probability 0
         F_loss =  (1-pt)**self.gamma * BCE_loss
         
